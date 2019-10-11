d08e2112e771ad51a1c54511e7f703b3e1688e82
KP9KBA <bazanova01@bk.ru>
2019-10-11 13:52:46 +0300

    LabaAlgo 3
    Задание 3

---
+++ LabaAlgo 3/zadanie 3.3/zadanie 3.3/zadanie 3.3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(void) {
+	setlocale(LC_ALL, "ru");
+	int N, i, j;
+	cout << "" << endl;
+	cin >> N;
+	for (int i = 1; i <= 9; ++i)// способ вывода столбцами 
+		//setw не читаются. Почему?
+	{
+		for (int j = 1; j <= 9; ++j)
+		{
+			cout << setw(2) << i * j << ' ';
+		}
+		cout << '\n';
+	}
+
+	/*cout << endl << setw(20) << left << dannoe1 << setw(20) << left << dannoe2 << setw(20) << left << dannoe3*/
+
+
+
+
+
+
+	return 0;
+}
