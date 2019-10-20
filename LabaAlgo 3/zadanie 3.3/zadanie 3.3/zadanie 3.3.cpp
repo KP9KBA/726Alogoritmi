442a444cefd4c6b794ba90afc02bee6e55723e59
KP9KBA <bazanova01@bk.ru>
2019-10-20 23:08:20 +0300

    Задание 3
    Матрица, но нужно проверить точность

--- LabaAlgo 3/zadanie 3.3/zadanie 3.3/zadanie 3.3.cpp
+++ LabaAlgo 3/zadanie 3.3/zadanie 3.3/zadanie 3.3.cpp
@@ -4,23 +4,34 @@ using namespace std;
 
 int main(void) {
 	setlocale(LC_ALL, "ru");
+
 	int N, i, j;
-	cout << "" << endl;
+	cout << "Введите число N" << endl;
 	cin >> N;
-	for (int i = 1; i <= 9; ++i)// способ вывода столбцами 
-		//setw не читаются. Почему?
+	for (int i = 1; i <= N; ++i)// способ вывода столбцами 
 	{
-		for (int j = 1; j <= 9; ++j)
+		for (int j = 1; j <= N; ++j)
 		{
-			cout << setw(2) << i * j << ' ';
+			cout << /*size_t() <<*/ i * j << ' '; //sizeof() - возвращает размер указателя
 		}
 		cout << '\n';
 	}
-
+	
 	/*cout << endl << setw(20) << left << dannoe1 << setw(20) << left << dannoe2 << setw(20) << left << dannoe3*/
-
-
-
+	/*int N;
+	cout << "Введите число N" << endl;
+	cin >> N;
+	int m_x[N];
+	int m_y[N];
+	int i, j, Z[m_x][m_y];
+	for (j = 0; j < N; j++) {
+		for (i = 0; i < (N - 1); i++) {
+			Z[N][N] = m_x[j] * m_y[i];
+			cout << Z[j][i];
+		}
+	}
+	*/
+	
 
 
 
